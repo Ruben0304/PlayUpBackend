f0baa17217df4a5aafbbd98e29fa012ae0fc4fee
Ale Diaz <ale.24dev@gmail.com>
2025-03-05 16:31:46 -0500

    add push notifications

--- api/api.py
+++ api/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Query, Request
 from services.country_service import CountryService
+from services.notification_service import NotificationService
 from services.tournament_season import TournamentSeasonService
-
+from services.user_service import UserService
 
 router = APIRouter()
 
@@ -23,3 +24,15 @@ async def create_bracket(request: Request):
 async def update_standing_rank(request: Request):
     payload = await request.json()
     return TournamentSeasonService.update_standing_rank(payload)
+
+@router.post("/push-notification")
+async def push_notification(request: Request):
+    payload = await request.json()
+    return NotificationService.push_notification(payload)
+
+@router.post("/approve-organizer-from-waitlist")
+async def approve_organizer_from_waitlist(request: Request):
+    payload = await request.json()
+    return UserService.approve_organizer_from_waitlist(payload['user_id'])
+
+
