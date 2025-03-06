06315a7ee203398487003faf765b5d6fdf3069a5
Ruben Hernandez <98282371+Ruben0304@users.noreply.github.com>
2025-03-06 11:12:29 -0500

    Update supabase_client.py

--- infrastructure/supabase_client.py
+++ infrastructure/supabase_client.py
@@ -3,8 +3,6 @@ from supabase import create_client, Client
 from core.config import SUPABASE_URL, SUPABASE_KEY
 
 class SupabaseClient:
-    SUPABASE_URL = os.getenv("SUPABASE_URL")
-    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
     client: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
     @staticmethod
